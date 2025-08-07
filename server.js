e076ef86ba5d1a4a834000227084885833ecbbea
lyricalnovaa <lyricalnovaprod@gmail.com>
2025-08-06 19:17:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -267,7 +267,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
